33b186c9edaf16db8f5e2a87f4cfda14d0215c32
Edward Luo <edward.luo@mail.utoronto.ca>
2021-06-10 12:32:04 -0400

    Additional setup

---
+++ QRDetectionInAR/ViewController+ARSessionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController+ARSessionDelegate.swift
+//  QRDetectionInAR
+//
+//  Created by Edward Luo on 2021-06-10.
+//
+
+import ARKit
+import Foundation
+
+extension ViewController: ARSessionDelegate {
+    
+}
--- QRDetectionInAR/ViewController.swift
+++ QRDetectionInAR/ViewController.swift
@@ -18,15 +18,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
